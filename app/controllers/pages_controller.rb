6b0ec99e0417d5c1b650b38da87f3a6b1eb82448
Ben D'Angelo <ben@bendangelo.me>
2026-08-12 20:18:05 -0400

    feat: add PagesController with homepage and privacy_and_terms
    Controller renders static pages. Routes: root -> pages#index,
    GET /privacy_and_terms -> pages#privacy_and_terms.
    
    Tests assert 200 responses and logo alt text presence.

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,7 @@
+class PagesController < ApplicationController
+  def index
+  end
+
+  def privacy_and_terms
+  end
+end
---
+++ test/controllers/pages_controller_test.rb
@@ -0,0 +1,16 @@
+require "test_helper"
+
+class PagesControllerTest < ActionDispatch::IntegrationTest
+  test "GET root renders homepage with logo" do
+    get root_path
+
+    assert_response :success
+    assert_select "img[alt=?]", "Stray Logo"
+  end
+
+  test "GET privacy_and_terms renders successfully" do
+    get privacy_and_terms_path
+
+    assert_response :success
+  end
+end
