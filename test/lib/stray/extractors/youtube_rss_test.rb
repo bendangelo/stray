87fabc5772491157fe1b2c43837337087409c977
Ben D'Angelo <ben@bendangelo.me>
2026-08-13 21:15:03 -0400

    feat: add YouTube RSS extractor with Feedjira

---
+++ test/lib/stray/extractors/youtube_rss_test.rb
@@ -0,0 +1,53 @@
+require "test_helper"
+
+class Stray::Extractors::YoutubeRssTest < ActiveSupport::TestCase
+  FIXTURE_PATH = File.expand_path("../../../fixtures/files/youtube_rss.xml", __dir__)
+
+  test "matches? returns true for YouTube RSS feed URLs" do
+    assert Stray::Extractors::YoutubeRss.matches?("https://www.youtube.com/feeds/videos.xml?channel_id=UC123")
+    assert Stray::Extractors::YoutubeRss.matches?("https://youtube.com/feeds/videos.xml?channel_id=UC123")
+  end
+
+  test "matches? returns false for non-RSS YouTube URLs" do
+    assert_not Stray::Extractors::YoutubeRss.matches?("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+    assert_not Stray::Extractors::YoutubeRss.matches?("https://www.youtube.com/@channel")
+    assert_not Stray::Extractors::YoutubeRss.matches?("https://www.youtube.com/channel/UC123")
+  end
+
+  test "matches? returns false for non-YouTube URLs" do
+    assert_not Stray::Extractors::YoutubeRss.matches?("https://example.com/feed.xml")
+    assert_not Stray::Extractors::YoutubeRss.matches?("https://bitchute.com/channel/abc")
+  end
+
+  test "extract returns array of ExtractedContent from RSS feed" do
+    rss_xml = File.read(FIXTURE_PATH)
+    stub_request(:get, /youtube\.com\/feeds\/videos\.xml/)
+      .to_return(body: rss_xml, headers: { "Content-Type" => "application/atom+xml" })
+
+    extractor = Stray::Extractors::YoutubeRss.new
+    results = extractor.extract("https://www.youtube.com/feeds/videos.xml?channel_id=UCuAXFkgsw1L7xaCfnd5JJOw")
+
+    assert_equal 2, results.size
+    first = results.first
+    assert_equal "dQw4w9WgXcQ", first.external_id
+    assert_equal "Rick Astley - Never Gonna Give You Up (Official Music Video)", first.title
+    assert_equal "The official video for Never Gonna Give You Up by Rick Astley.", first.content_text
+    assert_equal "https://i.ytimg.com/vi/dQw4w9WgXcQ/mqdefault.jpg", first.thumbnail_url
+    assert_equal Time.parse("2009-10-25T00:00:00+00:00"), first.published_at
+    assert_nil first.duration
+  end
+
+  test "extract includes creator_identity from feed author" do
+    rss_xml = File.read(FIXTURE_PATH)
+    stub_request(:get, /youtube\.com\/feeds\/videos\.xml/)
+      .to_return(body: rss_xml, headers: { "Content-Type" => "application/atom+xml" })
+
+    extractor = Stray::Extractors::YoutubeRss.new
+    results = extractor.extract("https://www.youtube.com/feeds/videos.xml?channel_id=UCuAXFkgsw1L7xaCfnd5JJOw")
+
+    creator = results.first.creator_identity
+    assert_equal "Rick Astley", creator.name
+    assert_equal "https://www.youtube.com/channel/UCuAXFkgsw1L7xaCfnd5JJOw", creator.url
+    assert_equal "UCuAXFkgsw1L7xaCfnd5JJOw", creator.external_id
+  end
+end
