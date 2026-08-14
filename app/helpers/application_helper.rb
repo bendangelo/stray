501f19da5db0d98e57e2562b1c059e0ecfdee6ee
Ben D'Angelo <ben@bendangelo.me>
2026-08-13 22:34:15 -0400

    feat: add time_ago helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+  def time_ago(time)
+    return "" if time.nil?
+
+    seconds = Time.current - time
+    if seconds < 60
+      "just now"
+    elsif seconds < 3600
+      "#{(seconds / 60).to_i}m ago"
+    elsif seconds < 86400
+      "#{(seconds / 3600).to_i}h ago"
+    elsif seconds < 604800
+      "#{(seconds / 86400).to_i}d ago"
+    else
+      time.strftime("%b %d, %Y")
+    end
+  end
 end
