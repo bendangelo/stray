b11355de015f3f61418fb493a4850788c637b06b
Ben D'Angelo <ben@bendangelo.me>
2026-08-13 21:15:33 -0400

    feat: add extractors initializer with registry registration

---
+++ config/initializers/extractors.rb
@@ -0,0 +1,5 @@
+Rails.application.config.to_prepare do
+  Stray::ExtractorRegistry.reset!
+  Stray::ExtractorRegistry.register(Stray::Extractors::YoutubeRss)
+  Stray::ExtractorRegistry.register(Stray::Extractors::YtDlp)
+end
