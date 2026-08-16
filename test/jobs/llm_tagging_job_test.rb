12466758a44139fdeafdc2295bacab28f4395fcd
Ben D'Angelo <ben@bendangelo.me>
2026-08-16 12:35:18 -0400

    test: convert llm tagging job tests to VCR cassettes

--- test/jobs/llm_tagging_job_test.rb
+++ test/jobs/llm_tagging_job_test.rb
@@ -29,11 +29,9 @@ class LlmTaggingJobTest < ActiveJob::TestCase
   end
 
   test "creates taggings from LLM response" do
-    response_body = { "choices" => [ { "message" => { "content" => '["ruby", "testing"]' } } ] }.to_json
-    stub_request(:post, "http://api:8080/v1/chat/completions")
-      .to_return(status: 200, body: response_body)
-
-    LlmTaggingJob.perform_now(@item.id)
+    VCR.use_cassette("llm_tagging/create_taggings") do
+      LlmTaggingJob.perform_now(@item.id)
+    end
 
     assert @item.taggings.where(source: :ai_llm).count >= 2
     assert Tag.exists?(user: users(:one), name: "ruby")
@@ -41,22 +39,19 @@ class LlmTaggingJobTest < ActiveJob::TestCase
   end
 
   test "creates new tags and enqueues embedding job" do
-    response_body = { "choices" => [ { "message" => { "content" => '["newtag"]' } } ] }.to_json
-    stub_request(:post, "http://api:8080/v1/chat/completions")
-      .to_return(status: 200, body: response_body)
-
-    assert_enqueued_with(job: EmbeddingJob) do
-      LlmTaggingJob.perform_now(@item.id)
+    VCR.use_cassette("llm_tagging/create_new_tags") do
+      assert_enqueued_with(job: EmbeddingJob) do
+        LlmTaggingJob.perform_now(@item.id)
+      end
     end
   end
 
   test "does not duplicate existing tags" do
     Tag.create!(user: users(:one), name: "existingtag")
-    response_body = { "choices" => [ { "message" => { "content" => '["existingtag"]' } } ] }.to_json
-    stub_request(:post, "http://api:8080/v1/chat/completions")
-      .to_return(status: 200, body: response_body)
 
-    LlmTaggingJob.perform_now(@item.id)
+    VCR.use_cassette("llm_tagging/no_duplicate") do
+      LlmTaggingJob.perform_now(@item.id)
+    end
 
     assert_equal 1, Tag.where(user: users(:one), name: "existingtag").count
   end
