293eaf6161ef41485c3d1b570cc86b9845ae76fb
Ben D'Angelo <ben@bendangelo.me>
2026-08-16 12:27:07 -0400

    test: convert openai-compatible embeddings test to VCR cassette

--- test/lib/stray/embeddings/providers/openai_compatible_test.rb
+++ test/lib/stray/embeddings/providers/openai_compatible_test.rb
@@ -12,19 +12,9 @@ class Stray::Embeddings::Providers::OpenAICompatibleTest < ActiveSupport::TestCa
   end
 
   test "embed calls OpenAI-compatible API and returns embedding" do
-    response_body = {
-      "data" => [ { "embedding" => [ 0.1, 0.2, 0.3 ] } ]
-    }.to_json
-
-    stub_request(:post, "http://api:8080/v1/embeddings")
-      .with(
-        body: { model: "text-embedding-3-small", input: "hello world" }.to_json,
-        headers: { "Authorization" => "Bearer test-key" }
-      )
-      .to_return(status: 200, body: response_body)
-
-    result = @provider.embed("hello world")
-
-    assert_equal [ 0.1, 0.2, 0.3 ], result
+    VCR.use_cassette("providers/openai_embeddings") do
+      result = @provider.embed("hello world")
+      assert_equal [ 0.1, 0.2, 0.3 ], result
+    end
   end
 end
