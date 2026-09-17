747a46ad9fe1739dc92af5d1577ab784d95d789a
Ben D'Angelo <ben@bendangelo.me>
2026-09-17 13:58:34 -0400

    test: dropdown menu assertions use computed styles

--- test/system/dropdown_menu_test.rb
+++ test/system/dropdown_menu_test.rb
@@ -10,41 +10,40 @@ class DropdownMenuTest < ApplicationSystemTestCase
       find("button[aria-controls^='item-actions-']").click
       within "div[data-dropdown-target='menu']" do
         link = find_link("Open details")
-        assert_equal "flex", link[:class].split.grep(/^flex/).first,
-                     "menu item link must be a flex row, got: #{link[:class]}"
-        refute link[:class].split.include?("dropdown_menu_item_class"),
-               "literal 'dropdown_menu_item_class' must not leak into markup"
+        assert_equal "flex", link.evaluate_script("getComputedStyle(this).display"),
+          "menu item link must be a flex row"
+        refute link[:class].include?("dropdown_menu_item_class"),
+          "literal helper name must not leak into markup"
       end
     end
   end
 
-  test "menu item link fills full menu width for reliable click target" do
+  test "menu item link fills full menu width and 44px height for reliable click target" do
     sign_in_as(users(:one))
     visit root_path
 
     within "##{dom_id(items(:video_one))}" do
       find("button[aria-controls^='item-actions-']").click
       menu = find("div[data-dropdown-target='menu']")
-      link = find_link("Open details")
 
       metrics = menu.evaluate_script(<<~JS)
         (() => {
           const s = getComputedStyle(this);
-          return {
-            content: this.clientWidth - parseFloat(s.paddingLeft) - parseFloat(s.paddingRight),
-            link: (() => {
-              const a = this.querySelector("a");
-              return a.getBoundingClientRect().width;
-            })()
-          };
+          const a = this.querySelector("a");
+          const r = a.getBoundingClientRect();
+          return { content: this.clientWidth - parseFloat(s.paddingLeft) - parseFloat(s.paddingRight), link: r.width };
         })()
       JS
       assert_in_delta metrics["content"], metrics["link"], 2,
-                      "clickable link should span menu content width, got link=#{metrics['link']} content=#{metrics['content']}"
+        "clickable link should span menu content width"
+
+      link = find_link("Open details")
+      height = link.evaluate_script("this.getBoundingClientRect().height")
+      assert_operator height, :>=, 44
     end
   end
 
-  test "sidebar source menu items use the helper classes" do
+  test "sidebar source menu items are 44px flex rows" do
     sign_in_as(users(:one))
     source = sources(:youtube)
     visit root_path
@@ -53,9 +52,9 @@ class DropdownMenuTest < ApplicationSystemTestCase
       find("button[aria-controls='sidebar-source-menu-#{source.id}']").click
       within "#sidebar-source-menu-#{source.id}" do
         link = find_link("Edit")
-        assert link[:class].split.include?("flex")
-        assert link[:class].split.include?("items-center")
-        assert link[:class].split.include?("w-full")
+        assert_equal "flex", link.evaluate_script("getComputedStyle(this).display")
+        height = link.evaluate_script("this.getBoundingClientRect().height")
+        assert_operator height, :>=, 44
       end
     end
   end
